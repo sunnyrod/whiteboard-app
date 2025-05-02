ec56ce30b93008bcc86a225af3bb49f36513162d
Sunny Rodriguez <107671425+sunnyrod@users.noreply.github.com>
2025-05-02 01:04:32 -0700

    feat: add ability to draw with pen

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,55 @@ const stage = new Konva.Stage({
 const layer = new Konva.Layer();
 stage.add(layer);
 
-const circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 50,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4,
-});
 
-layer.add(circle);
-layer.draw();
-
-document.getElementById('clear')?.addEventListener('click', () => {
+const clearWhiteboard = () => {
   layer.destroyChildren();
   layer.draw();
+};
+document.getElementById('clear')?.addEventListener('click', clearWhiteboard);
+
+// Start drawing
+let isDrawing = false;
+let currentLine: Konva.Line | null = null;
+const deleteLine = (e: Konva.KonvaEventObject<MouseEvent>) => {
+  e.currentTarget.destroy();
+  layer.draw();
+};
+stage.on('mousedown touchstart', (e) => {
+  if (e.evt.button !== 0) return; // Only left mouse button
+  if (e.target !== stage) return; // Only draw on the stage
+
+  isDrawing = true;
+
+  const pos = stage.getPointerPosition();
+  if (!pos) return;
+  currentLine = new Konva.Line({
+    points: [pos.x, pos.y],
+    stroke: 'black',
+    strokeWidth: 2,
+    lineCap: 'round',
+    lineJoin: 'round',
+  });
+  currentLine.on('click', deleteLine);
+  layer.add(currentLine);
+});
+
+// Continue drawing
+stage.on('mousemove touchmove', (e) => {
+  if (!isDrawing) return; // Only draw when mouse is down
+  if (e.evt.button !== 0) return; // Only left mouse button
+  if (e.target !== stage) return; // Only draw on the stage
+
+  const pos = stage.getPointerPosition();
+  if (!pos || !currentLine) return;
+
+  const newPoints = currentLine.points().concat([pos.x, pos.y]);
+  currentLine.points(newPoints);
+  layer.batchDraw();
+});
+
+// Stop drawing
+stage.on('mouseup touchend mouseleave touchleave', () => {
+  isDrawing = false;
+  currentLine = null;
 });
