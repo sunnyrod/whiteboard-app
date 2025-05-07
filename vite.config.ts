88703085777d2bc84180ed6f7d5d29c134064962
Sunny Rodriguez <107671425+sunnyrod@users.noreply.github.com>
2025-05-06 23:18:31 -0700

    build: fix assets not loading on github page
    The page is located at https://sunnyrod.github.io/whiteboard-app/ but the assets are trying to load
    from root directory `/`. The solution is to add the `whiteboard-app` prefix to the asset locations.

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/whiteboard-app/', // required for GitHub Pages using default project site
 })
