cfb8438c85167bd762631731a3122a72a2cd7df6
Sunny Rodriguez <107671425+sunnyrod@users.noreply.github.com>
2025-05-06 23:58:18 -0700

    ci: setup custom domain
    Followed instructions in http://tiny.cc/i6yi001

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/whiteboard-app/', // required for GitHub Pages using default project site
+  // required for GitHub Pages hosting at custom subdomain, see: http://tiny.cc/i6yi001
+  base: '/',
 })
